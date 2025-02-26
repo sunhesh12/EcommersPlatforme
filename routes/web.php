8c4e52f5a29e27f4b606555db9879ea837dd997b
sunhesh12 <hsdbandaranayake@gmail.com>
2025-02-26 22:33:42 +0530

    update routes

--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,12 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
+Route::get('/home', function () {
     return view('app/Home');
+});
+
+Route::get('/loginn', function () {
+    return view('app/login');
 });
 
 Route::get('/dashboard', function () {
