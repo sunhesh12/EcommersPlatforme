2c28b27f8283421a6003bb76b8a8a57ec715cd85
Sudesh Hansika <sudeshhansika@gmail.com>
2025-03-27 07:28:11 +0530

    My profile page development

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,14 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
+Route::get('/my-profile', function () {
+    return view('app/Myprofile');
+});
+
+Route::get('/shopping-cart', function () {
+    return view('app/ShoppingCart');
+});
+
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
