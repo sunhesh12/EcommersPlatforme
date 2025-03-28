38fcb73b3d255dd4d18954a22d69e1103097b052
sunhesh12 <hsdbandaranayake@gmail.com>
2025-03-28 22:48:53 +0530

    uodate nessary fetures

--- resources/views/app/layouts/main.blade.php
+++ resources/views/app/layouts/main.blade.php
@@ -16,13 +16,13 @@
     <link rel="stylesheet" href="{{ asset('css/cart.css') }}">
 <!-- <<<<<<< HEAD -->
     <link rel="stylesheet" href="{{ asset('css/login.css') }}">
-<<<<<<< HEAD
+<!-- <<<<<<< HEAD -->
 <!-- ======= -->
-    <link rel="stylesheet" href="{{ asset('css/register.css') }}">
+    <!-- <link rel="stylesheet" href="{{ asset('css/register.css') }}"> -->
 <!-- >>>>>>> origin/registerPageDevelopment -->
-=======
+<!-- ======= -->
     <link rel="stylesheet" href="{{ asset('css/about.css') }}">
->>>>>>> origin/aboutpagedevelopment
+<!-- >>>>>>> origin/aboutpagedevelopment -->
 
     <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/js/all.min.js" crossorigin="anonymous"></script>
 
