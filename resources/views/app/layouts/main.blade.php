fffdf3eaa7c25d0813a12b513f4ab98927d3bdbb
sunhesh12 <hsdbandaranayake@gmail.com>
2025-04-20 12:37:08 +0530

    remove product css as homepage css not work properly

--- resources/views/app/layouts/main.blade.php
+++ resources/views/app/layouts/main.blade.php
@@ -26,7 +26,9 @@
 <!-- >>>>>>> origin/aboutpagedevelopment -->
 
     <link rel="stylesheet" href="{{ asset('css/productDetails.css') }}">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- temmpary comment css file as homepage css is not working properly -->
+    <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet"> -->
    
 
 
