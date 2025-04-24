bd462c94eddc6ee762bc8542e3b6a55134e69d9a
Janith Nanayakkara <nanayakkaraj210@gmail.com>
2025-04-24 22:08:44 +0530

    update details into seeders

--- database/seeders/BrandSeeder.php
+++ database/seeders/BrandSeeder.php
@@ -20,5 +20,114 @@ class BrandSeeder extends Seeder
             'slug' => 'hp',
             'logo' => 'brand_logos/hp.png', // make sure this image exists in storage/public/brand_logos
         ]);
+
+        Brand::create([
+            'name' => 'Dell',
+            'slug' => 'dell',
+            'logo' => 'brand_logos/dell.png',
+        ]);
+        
+        
+        Brand::create([
+            'name' => 'Lenovo',
+            'slug' => 'lenovo',
+            'logo' => 'brand_logos/lenovo.png',
+        ]);
+        
+        Brand::create([
+            'name' => 'Acer',
+            'slug' => 'acer',
+            'logo' => 'brand_logos/acer.png',
+        ]);
+        
+        Brand::create([
+            'name' => 'Apple',
+            'slug' => 'apple',
+            'logo' => 'brand_logos/apple.png',
+        ]);
+        
+        Brand::create([
+            'name' => 'Microsoft',
+            'slug' => 'microsoft',
+            'logo' => 'brand_logos/microsoft.png',
+        ]);
+        
+        Brand::create([
+            'name' => 'MSI',
+            'slug' => 'msi',
+            'logo' => 'brand_logos/msi.png',
+        ]);
+        
+        Brand::create([
+            'name' => 'Razer',
+            'slug' => 'razer',
+            'logo' => 'brand_logos/razer.png',
+        ]);
+        
+        Brand::create([
+            'name' => 'Samsung',
+            'slug' => 'samsung',
+            'logo' => 'brand_logos/samsung.png',
+        ]);
+        
+        Brand::create([
+            'name' => 'Huawei',
+            'slug' => 'huawei',
+            'logo' => 'brand_logos/huawei.png',
+        ]);
+        
+        Brand::create([
+            'name' => 'Gigabyte',
+            'slug' => 'gigabyte',
+            'logo' => 'brand_logos/gigabyte.png',
+        ]);
+        
+        Brand::create([
+            'name' => 'Toshiba',
+            'slug' => 'toshiba',
+            'logo' => 'brand_logos/toshiba.png',
+        ]);
+        
+        Brand::create([
+            'name' => 'Panasonic',
+            'slug' => 'panasonic',
+            'logo' => 'brand_logos/panasonic.png',
+        ]);
+        
+        Brand::create([
+            'name' => 'LG',
+            'slug' => 'lg',
+            'logo' => 'brand_logos/lg.png',
+        ]);
+        
+        Brand::create([
+            'name' => 'Fujitsu',
+            'slug' => 'fujitsu',
+            'logo' => 'brand_logos/fujitsu.png',
+        ]);
+        
+        Brand::create([
+            'name' => 'VAIO',
+            'slug' => 'vaio',
+            'logo' => 'brand_logos/vaio.png',
+        ]);
+        
+        Brand::create([
+            'name' => 'Xiaomi',
+            'slug' => 'xiaomi',
+            'logo' => 'brand_logos/xiaomi.png',
+        ]);
+        
+        Brand::create([
+            'name' => 'Chuwi',
+            'slug' => 'chuwi',
+            'logo' => 'brand_logos/chuwi.png',
+        ]);
+        
+        Brand::create([
+            'name' => 'Realme',
+            'slug' => 'realme',
+            'logo' => 'brand_logos/realme.png',
+        ]);
     }
 }
--- database/seeders/EUserSeeder.php
+++ database/seeders/EUserSeeder.php
@@ -23,5 +23,145 @@ class EUserSeeder extends Seeder
             'country' => 'Sri Lanka',
             'postal_code' => '10100',
         ]);
+
+        EUser::create([
+            'name' => 'Nimal Perera',
+            'email' => 'nimal.perera@example.com',
+            'password' => Hash::make('password123'),
+            'accepted_terms' => true,
+            'is_admin' => false,
+            'phone' => '0711111111',
+            'address' => '10 Temple Road',
+            'city' => 'Kandy',
+            'state' => 'Central',
+            'country' => 'Sri Lanka',
+            'postal_code' => '20000',
+        ]);
+    
+        EUser::create([
+            'name' => 'Sunethra Silva',
+            'email' => 'sunethra.silva@example.com',
+            'password' => Hash::make('password123'),
+            'accepted_terms' => true,
+            'is_admin' => false,
+            'phone' => '0722222222',
+            'address' => '20 Lake View',
+            'city' => 'Galle',
+            'state' => 'Southern',
+            'country' => 'Sri Lanka',
+            'postal_code' => '80000',
+        ]);
+    
+        EUser::create([
+            'name' => 'Kamal Jayasinghe',
+            'email' => 'kamal.jayasinghe@example.com',
+            'password' => Hash::make('password123'),
+            'accepted_terms' => true,
+            'is_admin' => false,
+            'phone' => '0733333333',
+            'address' => '45 Flower Street',
+            'city' => 'Kurunegala',
+            'state' => 'North Western',
+            'country' => 'Sri Lanka',
+            'postal_code' => '60000',
+        ]);
+    
+        EUser::create([
+            'name' => 'Tharushi Dissanayake',
+            'email' => 'tharushi.dissanayake@example.com',
+            'password' => Hash::make('password123'),
+            'accepted_terms' => true,
+            'is_admin' => false,
+            'phone' => '0744444444',
+            'address' => '88 Hill Side',
+            'city' => 'Matale',
+            'state' => 'Central',
+            'country' => 'Sri Lanka',
+            'postal_code' => '21000',
+        ]);
+    
+        EUser::create([
+            'name' => 'Ruwan Fernando',
+            'email' => 'ruwan.fernando@example.com',
+            'password' => Hash::make('password123'),
+            'accepted_terms' => true,
+            'is_admin' => false,
+            'phone' => '0755555555',
+            'address' => '7 Main Street',
+            'city' => 'Negombo',
+            'state' => 'Western',
+            'country' => 'Sri Lanka',
+            'postal_code' => '11500',
+        ]);
+    
+        EUser::create([
+            'name' => 'Nadeesha Wijeratne',
+            'email' => 'nadeesha.wijeratne@example.com',
+            'password' => Hash::make('password123'),
+            'accepted_terms' => true,
+            'is_admin' => false,
+            'phone' => '0766666666',
+            'address' => '33 Palm Grove',
+            'city' => 'Gampaha',
+            'state' => 'Western',
+            'country' => 'Sri Lanka',
+            'postal_code' => '11000',
+        ]);
+    
+        EUser::create([
+            'name' => 'Isuru Madushan',
+            'email' => 'isuru.madushan@example.com',
+            'password' => Hash::make('password123'),
+            'accepted_terms' => true,
+            'is_admin' => false,
+            'phone' => '0777777777',
+            'address' => '12 Sea Road',
+            'city' => 'Trincomalee',
+            'state' => 'Eastern',
+            'country' => 'Sri Lanka',
+            'postal_code' => '31000',
+        ]);
+    
+        EUser::create([
+            'name' => 'Dilani Rathnayake',
+            'email' => 'dilani.rathnayake@example.com',
+            'password' => Hash::make('password123'),
+            'accepted_terms' => true,
+            'is_admin' => false,
+            'phone' => '0788888888',
+            'address' => '51 Lotus Park',
+            'city' => 'Ratnapura',
+            'state' => 'Sabaragamuwa',
+            'country' => 'Sri Lanka',
+            'postal_code' => '70000',
+        ]);
+    
+        EUser::create([
+            'name' => 'Chathura Senanayake',
+            'email' => 'chathura.senanayake@example.com',
+            'password' => Hash::make('password123'),
+            'accepted_terms' => true,
+            'is_admin' => false,
+            'phone' => '0799999999',
+            'address' => '99 Green Hills',
+            'city' => 'Anuradhapura',
+            'state' => 'North Central',
+            'country' => 'Sri Lanka',
+            'postal_code' => '50000',
+        ]);
+    
+        EUser::create([
+            'name' => 'Hiruni Bandara',
+            'email' => 'hiruni.bandara@example.com',
+            'password' => Hash::make('password123'),
+            'accepted_terms' => true,
+            'is_admin' => false,
+            'phone' => '0700000000',
+            'address' => '65 Rose Lane',
+            'city' => 'Badulla',
+            'state' => 'Uva',
+            'country' => 'Sri Lanka',
+            'postal_code' => '90000',
+        ]);
     }
 }
